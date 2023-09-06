f2091ea2eb566da424197e0cb2d10bf73f9914ab
mudassarali964 <55048197+mudassarali964@users.noreply.github.com>
2023-09-06 15:43:49 -0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'email',
         'password',
         'is_admin',
+        'is_active',
     ];
 
     /**
@@ -42,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
 }
