0ac3d560b4efb11ebac2a7f55e9729f4b66e2546
mudassarali964 <55048197+mudassarali964@users.noreply.github.com>
2023-09-06 15:40:11 -0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,16 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $numberOfUsers = 2;
-        for ($i = 0; $i<$numberOfUsers; $i++){
-            \App\Models\User::create([
-                'name' => $i == 0 ? 'admin' : 'user',
-                'email' => $i == 0 ? 'admin@admin.com' : 'user@user.com',
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'is_admin' => $i == 0 ?? false,
-                'remember_token' => Str::random(10),
-            ]);
-        }
+        (new UserSeeder())->run();
+        (new PostSeeder())->run();
     }
 }
